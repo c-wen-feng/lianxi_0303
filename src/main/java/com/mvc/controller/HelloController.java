62f0e4bf668f0fe75bd0896ef354090b52c29012
c-wen-feng <275242710@qq.com>
2022-03-17 16:34:05 +0800

    Initial commit

---
+++ src/main/java/com/mvc/controller/HelloController.java
@@ -0,0 +1,18 @@
+package com.mvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+
+    @RequestMapping("/")
+    public String index(){
+
+        return "index";
+    }
+    @RequestMapping("/target")
+    public String toTarget(){
+        return "target";
+    }
+}
